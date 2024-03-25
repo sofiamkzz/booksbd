b2478ffc5fcdf461dfadc68568ef8c7d1fc4d05e
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-03-25 17:31:30 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'at',
+    password: 'pass',
     database: 'library'
 });
 // Connect to MySQL
