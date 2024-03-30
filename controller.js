19f01dab104cf86438bd2b8c9fd7e9acca5159f8
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-03-30 10:05:22 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
-const userModel = require('./models');
+const bookModel = require('./models');
 
-// Rota para renderizar a página inicial com os resultados
+// Função para renderizar a página inicial com os resultados
 function renderHome(req, res) {
-    userModel.getAllBooks((err, results) => {
+    bookModel.getAllBooks((err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
@@ -11,37 +11,29 @@ function renderHome(req, res) {
     });
 }
 
-// Rota para exibir informações sobre um livro por ano a partir do parâmetro URL
+// Função para exibir informações sobre um livro por ano a partir do parâmetro URL
 function renderByYear(req, res) {
     const byear = req.params.year;
-    userModel.searchByYear(byear, (err, results) => {
+    bookModel.searchByYear(byear, (err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
-        }
-
-        if (results.length === 0) {
-            return res.render('index', { books: [], error: 'No books found.' });
         }
 
         res.render('index', { books: results, error: null, });
     });
 }  
 
-// Rota para pesquisar livros com base no título da string de consulta
+// Função para pesquisar livros com base no título da string de consulta
 function renderByTitle(req, res) {
     const title = req.query.title;
-    userModel.searchByTitle(title, (err, results) => {
+    bookModel.searchByTitle(title, (err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
         }
 
-        if (title) {
-            res.render('index', { books: results, error: null });
-        } else {
-             return res.render('index', { books: [], error: 'No books found.' });
-        }
+        res.render('index', { books: results, error: null });
     });
 }
 
